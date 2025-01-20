8a31d120193257634c90afd395dfa1844407cd8f
yihim <yihim_1999@hotmail.com>
2025-01-21 00:26:48 +0800

    added docling function

--- vectorstore/save_vectorstore.py
+++ vectorstore/save_vectorstore.py
@@ -17,6 +17,9 @@ import json
 import pandas as pd
 import re
 import warnings
+from docling.datamodel.base_models import InputFormat
+from docling.document_converter import DocumentConverter, PdfFormatOption
+from docling.datamodel.pipeline_options import PdfPipelineOptions, TableFormerMode
 
 warnings.filterwarnings("ignore")
 
@@ -25,30 +28,42 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 def save_vector_to_store(path: str, data_path: str):
     try:
         # embedding_model = SentenceTransformer(EMBEDDING_MODEL, trust_remote_code=True, device=device)
-        name_without_suff = os.path.basename(data_path).split(".")[0]
+        # name_without_suff = os.path.basename(data_path).split(".")[0]
+        #
+        # local_image_dir, local_md_dir = "./output/images", "output"
+        # image_dir = str(os.path.basename(local_image_dir))
+        #
+        # os.makedirs(local_md_dir, exist_ok=True)
+        #
+        # image_writer, md_writer = FileBasedDataWriter(
+        #     local_image_dir
+        # ), FileBasedDataWriter(local_md_dir)
+        #
+        # reader1 = FileBasedDataReader("")
+        # pdf_bytes = reader1.read(data_path)
+        #
+        # ds = PymuDocDataset(pdf_bytes)
+        #
+        # if ds.classify() == SupportedPdfParseMethod.OCR:
+        #     ds.apply(doc_analyze, ocr=True).pipe_ocr_mode(image_writer).dump_md(
+        #         md_writer, f"{name_without_suff}.md", image_dir
+        #     )
+        # else:
+        #     ds.apply(doc_analyze, ocr=False).pipe_txt_mode(image_writer).dump_md(
+        #         md_writer, f"{name_without_suff}.md", image_dir
+        #     )
 
-        local_image_dir, local_md_dir = "./output/images", "output"
-        image_dir = str(os.path.basename(local_image_dir))
+        pipeline_options = PdfPipelineOptions(do_table_structure=True, do_ocr=True)
+        pipeline_options.table_structure_options.mode = TableFormerMode.ACCURATE  # use more accurate TableFormer model
+        pipeline_options.table_structure_options.do_cell_matching = True
 
-        os.makedirs(local_md_dir, exist_ok=True)
-
-        image_writer, md_writer = FileBasedDataWriter(
-            local_image_dir
-        ), FileBasedDataWriter(local_md_dir)
-
-        reader1 = FileBasedDataReader("")
-        pdf_bytes = reader1.read(data_path)
-
-        ds = PymuDocDataset(pdf_bytes)
-
-        if ds.classify() == SupportedPdfParseMethod.OCR:
-            ds.apply(doc_analyze, ocr=True).pipe_ocr_mode(image_writer).dump_md(
-                md_writer, f"{name_without_suff}.md", image_dir
-            )
-        else:
-            ds.apply(doc_analyze, ocr=False).pipe_txt_mode(image_writer).dump_md(
-                md_writer, f"{name_without_suff}.md", image_dir
-            )
+        doc_converter = DocumentConverter(
+            format_options={
+                InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)
+            }
+        )
+        result = doc_converter.convert(data_path)
+        print(result.document.export_to_markdown())
 
     except Exception as e:
         print(f"Unexpected error occurred in save_vector_to_store function: {e}")
@@ -57,5 +72,5 @@ def save_vector_to_store(path: str, data_path: str):
 if __name__ == "__main__":
     save_vector_to_store(
         path="../vectordb_chroma/",
-        data_path="../data/Pdf/2ED27NR7CISW7J4PHXXBZ6OFPVDFHMFB.pdf",
+        data_path="../data/Pdf/3OHZBJVNLM4IAL6GLY3AKUQIVCNLPJLO.pdf",
     )
