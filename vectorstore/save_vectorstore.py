72fcef80322642549d97e88b8af3117259b49d53
yihim <yihim_1999@hotmail.com>
2025-01-21 23:10:54 +0800

    removed docling and magic-pdf function, integrated using api for data extraction with magic-pdf

--- vectorstore/save_vectorstore.py
+++ vectorstore/save_vectorstore.py
@@ -1,76 +1,106 @@
 from langchain_community.vectorstores import Chroma
-from langchain_ollama import OllamaEmbeddings
 from langchain.schema import Document
 import os
-import pprint
-from magic_pdf.config.enums import SupportedPdfParseMethod
 from sentence_transformers import SentenceTransformer
-from sentence_transformers.util import cos_sim
 import torch
-from constants.models import EMBEDDING_MODEL
-from magic_pdf.data.data_reader_writer import FileBasedDataWriter, FileBasedDataReader
-from langchain_community.document_loaders import UnstructuredMarkdownLoader
-from magic_pdf.data.dataset import PymuDocDataset
-from magic_pdf.model.doc_analyze_by_custom_model import doc_analyze
-from bs4 import BeautifulSoup
-import json
-import pandas as pd
-import re
+from constants.models import (
+    EMBEDDING_MODEL,
+    TABLE_ORGANIZER_LLM,
+    TABLE_ORGANIZER_LLM_SYSTEM_PROMPT,
+)
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from utils.models import quant_config_4_bit
+from typing import Tuple
+from dotenv import load_dotenv
+import requests
+import shutil
+from huggingface_hub import login
 import warnings
-from docling.datamodel.base_models import InputFormat
-from docling.document_converter import DocumentConverter, PdfFormatOption
-from docling.datamodel.pipeline_options import PdfPipelineOptions, TableFormerMode
 
 warnings.filterwarnings("ignore")
 
+load_dotenv()
+login(os.getenv("HF_TOKEN_WRITE"), add_to_git_credential=True)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
+
+def load_llm_and_tokenizer() -> (
+    Tuple[AutoModelForCausalLM.from_pretrained, AutoTokenizer.from_pretrained]
+):
+    tokenizer = AutoTokenizer.from_pretrained(
+        TABLE_ORGANIZER_LLM, trust_remote_code=True
+    )
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"
+
+    llm = AutoModelForCausalLM.from_pretrained(
+        TABLE_ORGANIZER_LLM, quantization_config=quant_config_4_bit(), device_map=device
+    )
+
+    llm.generation_config.pad_token_id = tokenizer.pad_token_id
+
+    return llm, tokenizer
+
+
+def make_llm_input_ids(tokenizer: AutoTokenizer.from_pretrained, context: str):
+    messages = [
+        {"role": "system", "content": TABLE_ORGANIZER_LLM_SYSTEM_PROMPT},
+        {
+            "role": "user",
+            "content": f"Below is the HTML formatted table:\n{context}\nPlease organize it.",
+        },
+    ]
+
+    messages = tokenizer.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True
+    )
+
+    model_inputs = tokenizer([messages], return_tensors="pt").to(device)
+
+    return model_inputs
+
+
+def clean_and_organize_external_data(extracted_data_path: str):
+    with open(extracted_data_path, "r", encoding="utf-8") as f:
+        extracted_data = f.read()
+
+    extracted_data_list = [data for data in extracted_data.split("\n") if data]
+
+    llm, tokenizer = load_llm_and_tokenizer()
+
+    for data in extracted_data_list:
+        if data.startswith("<html><body><table>"):
+            input_ids = make_llm_input_ids(tokenizer, data)
+            generated_ids = llm.generate(**input_ids, max_new_tokens=1024)
+            generated_ids = [
+                output_ids[len(input_ids):]
+                for input_ids, output_ids in zip(input_ids.input_ids, generated_ids)
+            ]
+            response = tokenizer.decode(generated_ids, skip_special_tokens=True)[0]
+            print(response)
+
+def extract_data_from_source(data_path: str):
+    file_name = os.path.basename(data_path)
+    file_name_without_ext = file_name.split(".")[0]
+    mounted_dir = "../data_extraction/data/"
+    shutil.copy(data_path, mounted_dir)
+    response = requests.post("http://localhost:8000/extract", json={'file_path': f"./data/{file_name}"})
+    if response.status_code == 200:
+        extracted_md_data_path = f"../data_extraction/tmp/md/{file_name_without_ext}.md"
+        with open(extracted_md_data_path, "r", encoding="utf-8") as f:
+            extracted_data = f.read()
+
+        print(extracted_data)
+
+
 def save_vector_to_store(path: str, data_path: str):
-    try:
-        # embedding_model = SentenceTransformer(EMBEDDING_MODEL, trust_remote_code=True, device=device)
-        # name_without_suff = os.path.basename(data_path).split(".")[0]
-        #
-        # local_image_dir, local_md_dir = "./output/images", "output"
-        # image_dir = str(os.path.basename(local_image_dir))
-        #
-        # os.makedirs(local_md_dir, exist_ok=True)
-        #
-        # image_writer, md_writer = FileBasedDataWriter(
-        #     local_image_dir
-        # ), FileBasedDataWriter(local_md_dir)
-        #
-        # reader1 = FileBasedDataReader("")
-        # pdf_bytes = reader1.read(data_path)
-        #
-        # ds = PymuDocDataset(pdf_bytes)
-        #
-        # if ds.classify() == SupportedPdfParseMethod.OCR:
-        #     ds.apply(doc_analyze, ocr=True).pipe_ocr_mode(image_writer).dump_md(
-        #         md_writer, f"{name_without_suff}.md", image_dir
-        #     )
-        # else:
-        #     ds.apply(doc_analyze, ocr=False).pipe_txt_mode(image_writer).dump_md(
-        #         md_writer, f"{name_without_suff}.md", image_dir
-        #     )
-
-        pipeline_options = PdfPipelineOptions(do_table_structure=True, do_ocr=True)
-        pipeline_options.table_structure_options.mode = TableFormerMode.ACCURATE  # use more accurate TableFormer model
-        pipeline_options.table_structure_options.do_cell_matching = True
-
-        doc_converter = DocumentConverter(
-            format_options={
-                InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)
-            }
-        )
-        result = doc_converter.convert(data_path)
-        print(result.document.export_to_markdown())
-
-    except Exception as e:
-        print(f"Unexpected error occurred in save_vector_to_store function: {e}")
+    # embedding_model = SentenceTransformer(EMBEDDING_MODEL, trust_remote_code=True, device=device)
+    extract_data_from_source(data_path)
+    # clean_and_organize_external_data(extracted_data_path)
 
 
 if __name__ == "__main__":
     save_vector_to_store(
         path="../vectordb_chroma/",
-        data_path="../data/Pdf/3OHZBJVNLM4IAL6GLY3AKUQIVCNLPJLO.pdf",
+        data_path="../data/Pdf/0a29925ccc5e6299e132a73325956a3abef6dd26.pdf",
     )
