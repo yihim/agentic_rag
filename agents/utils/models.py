47b9a5fbabad5097eb7cddaee83f16ed4932e28b
yihim <yihim_1999@hotmail.com>
2025-02-17 15:34:20 +0800

    added load llm and tokenizer function

--- agents/utils/models.py
+++ agents/utils/models.py
@@ -2,16 +2,21 @@ import random
 import numpy as np
 import torch
 from transformers import BitsAndBytesConfig
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+)
+from typing import Tuple
 
 # Reproducibility
-def set_random_seed(seed):
+def set_random_seed(seed: int):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = False
+        torch.backends.cudnn.benchmark = True
 
 # 4 Bit quantization
 def quant_config_4_bit():
@@ -28,3 +33,23 @@ def quant_config_8_bit():
         load_in_8bit=True,
         bnb_8bit_compute_dtype=torch.bfloat16
     )
+
+# Load llm and tokenizer
+def load_llm_and_tokenizer(llm_name: str, device: str) -> Tuple[
+    AutoModelForCausalLM.from_pretrained,
+    AutoTokenizer.from_pretrained,
+]:
+    llm = AutoModelForCausalLM.from_pretrained(
+        llm_name,
+        quantization_config=quant_config_4_bit(),
+        device_map=device,
+        torch_dtype=torch.bfloat16,
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(llm_name, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "left"
+
+    llm.generation_config.pad_token_id = tokenizer.pad_token_id
+
+    return llm, tokenizer
