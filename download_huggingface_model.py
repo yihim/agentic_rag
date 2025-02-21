92fc6a5b4a1624c9c371cef25e54c469c9c888dc
yihim <yihim_1999@hotmail.com>
2025-02-21 15:33:55 +0800

    updated

--- download_huggingface_model.py
+++ download_huggingface_model.py
@@ -1,12 +1,9 @@
 import os
-from transformers import AutoModelForCausalLM, AutoTokenizer
 from dotenv import load_dotenv
 from agents.constants.models import QWEN
-from huggingface_hub import login
+from huggingface_hub import login, snapshot_download
 
 if __name__ == "__main__":
     load_dotenv()
     login(os.getenv("HF_TOKEN_WRITE"), add_to_git_credential=True)
-    llm, tokenizer = AutoModelForCausalLM.from_pretrained(
-        QWEN, trust_remote_code=True
-    ), AutoTokenizer.from_pretrained(QWEN, trust_remote_code=True)
+    snapshot_download(repo_id=QWEN, trust_remote_code=True)
