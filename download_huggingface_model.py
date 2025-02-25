c14aa57459ae67c8193c4681bb7807ee6ee90165
yihim <yihim_1999@hotmail.com>
2025-02-26 07:56:39 +0800

    added download embedding model

--- download_huggingface_model.py
+++ download_huggingface_model.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
-from agents.constants.models import QWEN
+from agents.constants.models import QWEN, EMBEDDING_MODEL
 from huggingface_hub import login, snapshot_download
 
 if __name__ == "__main__":
     load_dotenv()
     login(os.getenv("HF_TOKEN_WRITE"), add_to_git_credential=True)
     snapshot_download(repo_id=QWEN, trust_remote_code=True)
+    snapshot_download(repo_id=EMBEDDING_MODEL, trust_remote_code=True)
