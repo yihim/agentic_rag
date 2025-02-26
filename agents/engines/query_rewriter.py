0ed5f9a5cc337f1636acf3541c86d0a9ee17ad83
yihim <yihim_1999@hotmail.com>
2025-02-26 15:02:02 +0800

    added query rewriter and response checker agents

---
+++ agents/engines/query_rewriter.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+import os
+from agents.constants.models import VLLM_BASE_URL
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+from langchain_core.messages import SystemMessage, HumanMessage
+from agents.constants.models import QUERY_REWRITER_SYSTEM_PROMPT
+
+
+class QueryRewriterOutput(BaseModel):
+    improved_query: str = Field(..., description="The rewritten query.")
+
+
+load_dotenv()
+
+client = ChatOpenAI(
+    base_url=VLLM_BASE_URL,
+    api_key=os.getenv("VLLM_API_KEY"),
+    model="/model",
+    verbose=True,
+    request_timeout=None,
+)
+
+structured_client = client.with_structured_output(QueryRewriterOutput)
+
+
+def main():
+    messages = [
+        SystemMessage(content=QUERY_REWRITER_SYSTEM_PROMPT),
+        HumanMessage(
+            content="I'm having trouble understanding the recent changes in our company's HR policies, especially regarding the new remote work procedures and benefits adjustments. Can you explain what has changed and how these updates might affect my daily workflow?"
+        ),
+    ]
+
+    response = structured_client.invoke(
+        input=messages,
+        temperature=0.01,
+        seed=42,
+        top_p=0.8,
+        max_tokens=1024,
+        extra_body={
+            "top_k": 20,
+            "repetition_penalty": 1,
+            "presence_penalty": 0,
+            "frequency_penalty": 0,
+        },
+    )
+
+    return response.improved_query
+
+
+if __name__ == "__main__":
+    # Test query rewriter
+    root_dir = Path(__file__).parent.parent.parent
+    os.chdir(root_dir)
+
+    print(main())
---
+++ agents/engines/response_checker.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+import os
+from agents.constants.models import VLLM_BASE_URL
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+from langchain_core.messages import SystemMessage
+from agents.constants.models import RESPONSE_CHECKER_SYSTEM_PROMPT
+
+
+class ResponseCheckerOutput(BaseModel):
+    valid_answer: str = Field(
+        ...,
+        description="This field must contain 'yes' if the provided answer fully addresses the user query, or 'no' if it does not.",
+    )
+
+
+load_dotenv()
+
+client = ChatOpenAI(
+    base_url=VLLM_BASE_URL,
+    api_key=os.getenv("VLLM_API_KEY"),
+    model="/model",
+    verbose=True,
+    request_timeout=None,
+)
+
+structured_client = client.with_structured_output(ResponseCheckerOutput)
+
+
+def main():
+    query = "How do I reset my forgotten email password?"
+    yes_answer = "To reset your forgotten email password, go to the login page and click on 'Forgot Password'. Enter your registered email address, complete any CAPTCHA or verification steps, and then check your inbox for a reset link. Follow the instructions in the email to create a new password."
+    no_answer = "Reset your password by contacting customer support."
+    messages = [
+        SystemMessage(
+            content=RESPONSE_CHECKER_SYSTEM_PROMPT.format(
+                query=query, answer=yes_answer
+            )
+        ),
+    ]
+
+    response = structured_client.invoke(
+        input=messages,
+        temperature=0.01,
+        seed=42,
+        top_p=0.8,
+        max_tokens=1024,
+        extra_body={
+            "top_k": 20,
+            "repetition_penalty": 1,
+            "presence_penalty": 0,
+            "frequency_penalty": 0,
+        },
+    )
+
+    return response.valid_answer
+
+
+if __name__ == "__main__":
+    # Test response checker
+    root_dir = Path(__file__).parent.parent.parent
+    os.chdir(root_dir)
+
+    print(main())
